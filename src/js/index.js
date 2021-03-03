6129581f655d4e186a19e49732d3f59e1186c0f9
Danben Erick <danbencb08@outlook.es>
2021-03-02 23:26:38 -0500

    Se termino el diseño de las paginas solo falta el responsive
    Se termino la configuracion e instalacion de dependencias para poder
    usar react en la app web

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Helo = () => <h1>Hola Mundo Prueba</h1>
+
+ReactDOM.render(<Helo />, document.querySelector("#app"))
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path')
 
 const config = {
-  entry : 'src/index.js',
-  mode: 'development',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
-  }
+    entry: './src/js/index.js',
+    output: {
+      filename: '[name].js',
+      path: path.join(__dirname, 'dist/js')
+    },
+    devServer: {
+      // open: true,
+      hot: true,
+      port : 9000
+    },
+    module: {
+      rules: [
+          {
+            test: /\.js|.jsx$/,
+            use: 'babel-loader'
+          },
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+          }
+      ]
+    }
 }
 
 module.exports = config
